3878f8a9ff1f765dc4125dc408dc36ac6fb75c66
lynne <user@email.com>
2021-09-20 19:58:37 -0700

    -add state for fetched posts - create toggleFetch to trigger useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
 import { baseURL, config } from "./services";
+import Nav from "./components/Nav";
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [toggleFetch, setToggleFetch] = useState(false);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await axios.get(baseURL, config);
+      setPosts(response.data.records);
+    }
+    getPosts();
+  }, [toggleFetch]);
+
   return (
     <div className="App">
+      <Nav />
       <Route exact path="/">
         <main>
         </main>
