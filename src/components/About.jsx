0e58a9daead04be254224df084dc3d30eb434469
lynne <user@email.com>
2021-09-27 08:47:56 -0700

    edit about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,7 @@ import "./About.css"
 function About() {
     return (
         <div className="about">
-            Bubble Tea (n):
-            <p>A tea-based drink of Taiwan origin served with tapioca balls or "boba" with a splash of happiness. </p>
+            What is Bubble Tea? It's a tea-based drink of Taiwan origin served with tapioca balls or "boba" with a splash of happiness! Join the conversation by sharing your own tea :)
         </div>
     
     )
